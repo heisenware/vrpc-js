7726ab04526561953af4a2e4c4a6d4763669d289
Burkhard Heisen <burkhard.heisen@xsmail.com>
2018-03-25 19:42:27 +0200

    Fixed setup.py to use correct vrpc path

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, Extension, find_packages
 
 module = Extension(
     'vrpc_test',
-    include_dirs=['./cpp', './test/fixtures'],
+    include_dirs=['./vrpc', './test/fixtures'],
     define_macros=[
         ('VRPC_COMPILE_AS_ADDON', '<binding.cpp>'),
         ('VRPC_MODULE_NAME', '"vrpc_test"'),
         ('VRPC_MODULE_FUNC', 'PyInit_vrpc_test')
     ],
     extra_compile_args=['-std=c++14', '-fPIC'],
-    sources=['./cpp/module.cpp'],
+    sources=['./vrpc/module.cpp'],
     language='c++'
 )
 
