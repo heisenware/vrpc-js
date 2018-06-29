72de2a2a15ea59e02e0f0fc535ff1c422b338ee9
Burkhard Heisen <burkhard.heisen@xsmail.com>
2018-06-29 14:54:10 +0200

    Corrected version information

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if environ.get('BUILD_EXAMPLE') == '1':
 
 setup(
     name='vrpc',
-    version='1.1.5',
+    version='2.0.0-alpha.0',
     license='MIT',
     description='Variadic Remote Procedure Calls',
     long_description=long_description,
