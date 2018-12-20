98cf328952b8e18e68942765f844c2fdef69e4ec
Burkhard Heisen <burkhard.heisen@xsmail.com>
2018-12-20 11:04:29 +0100

    Exposing VrpcAdapter

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 module.exports = {
   VrpcLocal: require('./vrpc/VrpcLocal'),
   VrpcRemote: require('./vrpc/VrpcRemote'),
-  VrpcAdapter: require('./vrpc/VrpcAdapter')
+  VrpcAdapter: require('./vrpc/VrpcAdapter'),
+  VrpcAgent: require('./vrpc/VrpcAgent')
 }
