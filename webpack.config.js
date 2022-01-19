c5318426885e5905992cbb37ee626fac66b77f05
Burkhard Heisen <burkhard.heisen@xsmail.com>
2022-01-19 15:08:04 +0100

    adding buffer and process to webpacked version

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
@@ -8,8 +9,9 @@ module.exports = {
       os: require.resolve('os-browserify/browser'),
       crypto: require.resolve('crypto-browserify'),
       url: require.resolve('url/'),
-      stream: require.resolve('stream-browserify')
-    },
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer')
+    }
   },
   output: {
     path: path.resolve(__dirname, 'browser'),
@@ -17,4 +19,14 @@ module.exports = {
     libraryTarget: 'umd',
     filename: 'vrpc.js'
   },
-};
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    })
+  ]
+}
