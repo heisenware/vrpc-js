5b68520f73c551197c7e041829b11ddcc7aee771
Burkhard Heisen <burkhard.heisen@xsmail.com>
2022-01-20 17:01:45 +0100

    Updated copyright notice

--- vrpc/addon.cpp
+++ vrpc/addon.cpp
@@ -15,7 +15,7 @@ Author: Dr. Burkhard C. Heisen (https://github.com/heisenware/vrpc)
 
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-Copyright (c) 2018 - 2021 Dr. Burkhard C. Heisen
+Copyright (c) 2018 - 2022 Dr. Burkhard C. Heisen
 <burkhard.heisen@heisenware.com>.
 
 Permission is hereby  granted, free of charge, to any  person obtaining a copy
