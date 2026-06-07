c536acdec0a789de906665dec2abb71ce71a45de
bheisen <burkhard.heisen@heisenware.com>
2026-06-07 22:26:09 +0200

    configure custom domain vrpc.io

--- website/vite.config.js
+++ website/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vrpc-js/', // Tells Vite to prepend this to all asset paths
+  base: '/',
   plugins: [react()],
   assetsInclude: ['**/*.md'],
   server: {
