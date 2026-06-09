09f935c629f61efc7422904b315c63a738929dc9
bheisen <burkhard.heisen@heisenware.com>
2026-06-09 14:08:09 +0200

    updated vrpc-hpp version

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -137,7 +137,7 @@ include(FetchContent)
 FetchContent_Declare(
   vrpc
   GIT_REPOSITORY https://github.com/heisenware/vrpc-hpp.git
-  GIT_TAG        v3.1.0
+  GIT_TAG        v3.1.1
 )
 FetchContent_MakeAvailable(vrpc)
 
